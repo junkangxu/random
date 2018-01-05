6cb9400aa9adea2b7e736cb952360f89cc452d00
junkangxu <david.xujunkang@gmail.com>
2018-01-05 16:53:13 +0800

    setup logics

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -2,19 +2,38 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import FlatButtonExampleSimple from './test';
 
 export default class AppNav extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    // this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      open: false,
+      type: 0
+    };
   }
 
   handleToggle = () => this.setState({open: !this.state.open});
 
   handleClose = () => this.setState({open: false});
 
+  handleClick(e) {
+    this.setState({open: false, type: e});
+  }
+
+  getContent() {
+    console.log(this.state.type);
+    if (this.state.type !== 0) {
+      return <FlatButtonExampleSimple />;
+    }
+    return <button />;
+  }
+
   render() {
+
     return (
       <div>
         <AppBar title="Random" onLeftIconButtonClick={this.handleToggle} />
@@ -24,7 +43,7 @@ export default class AppNav extends React.Component {
           open={this.state.open}
           onRequestChange={(open) => this.setState({open})}
         >
-          <MenuItem onClick={this.handleClose}>数字</MenuItem>
+          <MenuItem onClick={this.handleClick.bind(this, 1)}>数字</MenuItem>
           <MenuItem onClick={this.handleClose}>英文字母</MenuItem>
           <MenuItem onClick={this.handleClose}>骰子</MenuItem>
           <MenuItem onClick={this.handleClose}>是否</MenuItem>
@@ -36,6 +55,9 @@ export default class AppNav extends React.Component {
           <MenuItem onClick={this.handleClose}>剪刀石头布</MenuItem>
           <MenuItem onClick={this.handleClose}>手心手背</MenuItem>
         </Drawer>
+        <div className="content">
+          {this.getContent()}
+        </div>
       </div>
     );
   }
