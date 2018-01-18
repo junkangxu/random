c51ddadb385e3a3d688557adf92b2dc8c6168ca0
junkangxu <david.xujunkang@gmail.com>
2018-01-18 16:35:40 -0600

    add Google Map API

--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
+const ApiKey = "AIzaSyCzh__hj0x5ywWnMZTHDv02i_PgSZ1B9mw";
+
 export default class SimpleMap extends Component {
-  
+
   render() {
     return (
       <GoogleMapReact
+        bootstrapURLKeys={{key: ApiKey}}
         center={this.props.center}
         zoom={this.props.zoom}
       >
