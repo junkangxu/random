7851a39675aeb7b257816d04422a6560fae26115
junkangxu <david.xujunkang@gmail.com>
2018-01-23 14:06:16 -0600

    change theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { red500 } from 'material-ui/styles/colors';
 import AppNav from './components/AppNav';
 import './App.css';
+
+const muiTheme = getMuiTheme({
+  fontFamily: 'Arial Black, Gadget, sans-serif',
+  palette: {
+    primary1Color: red500
+  }
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MuiThemeProvider>
+        <MuiThemeProvider muiTheme={muiTheme}>
           <AppNav />
         </MuiThemeProvider>
       </div>
